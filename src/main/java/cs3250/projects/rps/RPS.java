933f94b0aee2e27e50246d7bcfa43905c955d956
Unknown <skyesladeut@outlook.com>
2019-09-12 20:52:40 -0600

    Created a Functioning RPS class and test cases

--- src/main/java/cs3250/projects/rps/RPS.java
+++ src/main/java/cs3250/projects/rps/RPS.java
@@ -1,5 +1,81 @@
 package cs3250.projects.rps;
 
 public class RPS {
-
+	public static String rpsWinner(String playerMoves) {
+		if(playerMoves == null) {
+			return "String is null";
+		}
+		else if(playerMoves == "") {
+			return "String is empty";
+		}
+		else {
+			String[] moves = playerMoves.toUpperCase().split(",");
+			int aWins = 0;
+			int bWins = 0;
+			for(int i = 0; i < moves.length; i++) {
+				moves[i] = moves[i].trim();
+				if(moves[i].length() != 2) {
+					return "String must contain 2 letters (RPS) seperated by commas";
+				}
+				switch(moves[i].charAt(0)) {
+				case 'R':
+					switch(moves[i].charAt(1)) {
+					case 'R':
+						//Draw
+						break;
+					case 'P':
+						bWins++;
+						break;
+					case 'S':
+						aWins++;
+						break;
+					default:
+						return "String must only contain letters R, P, and S";
+					}
+					break;
+				case 'P':
+					switch(moves[i].charAt(1)) {
+					case 'R':
+						aWins++;
+						break;
+					case 'P':
+						//Draw
+						break;
+					case 'S':
+						bWins++;
+						break;
+					default:
+						return "String must only contain letters R, P, and S";
+					}
+					break;
+				case 'S':
+					switch(moves[i].charAt(1)) {
+					case 'R':
+						bWins++;
+						break;
+					case 'P':
+						aWins++;
+						break;
+					case 'S':
+						//Draw
+						break;
+					default:
+						return "String must only contain letters R, P, and S";
+					}
+					break;
+				default:
+					return "String must only contain letters R, P, and S";
+				}
+			}
+			if(aWins == bWins) {
+				return "Draw";
+			}
+			else if(aWins > bWins) {
+				return "Player A Wins";
+			}
+			else {
+				return "Player B Wins";
+			}
+		}
+	}
 }
--- src/test/java/cs3250/projects/rps/RPSTest.java
+++ src/test/java/cs3250/projects/rps/RPSTest.java
@@ -1,14 +1,89 @@
 package cs3250.projects.rps;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
+
 public class RPSTest {
+	
 	//Null
+	@Test
+	public void testRPSWinner_null() {
+		assertEquals("String is null", RPS.rpsWinner(null) );
+	}
+	
 	//Empty String
-	//1 character string
-	//3+ character string
-	//non RPS characters
+	@Test
+	public void testRPSWinner_emptyString() {
+		assertEquals("String is empty", RPS.rpsWinner("") );
+	}
+	
+	//one character string
+	@Test
+	public void testRPSWinner_oneCharacterString() {
+		assertEquals("String must contain 2 letters (RPS) seperated by commas", RPS.rpsWinner("R, PS, RP, S"));
+	}
+	
+	//Three plus character string
+	@Test
+	public void testRPSWinner_threePlusCharacterString() {
+		assertEquals("String must contain 2 letters (RPS) seperated by commas", RPS.rpsWinner("RS, PSR, RP, SR"));
+	}
+	
+	//Non RPS Letter
+	@Test
+	public void testRPSWinner_nonRPSLetter() {
+		assertEquals("String must only contain letters R, P, and S", RPS.rpsWinner("RS, TR, RR, SS"));
+	}
+	
+	//Has Special Character
+	@Test
+	public void testRPSWinner_specialCharacter() {
+		assertEquals("String must only contain letters R, P, and S", RPS.rpsWinner("[S, PR, RR, SS"));
+	}
+	
+	//Has Numbers
+	@Test
+	public void testRPSWinner_numberCharacter() {
+		assertEquals("String must only contain letters R, P, and S", RPS.rpsWinner("RS, PR, R1, SS"));
+	}
+	
 	//Draw case
+	@Test
+	public void testRPSWinner_draw() {
+		assertEquals("Draw", RPS.rpsWinner("RS, SS, RP, PS, SP, RR"));
+	}
+	
 	//A wins
+	@Test
+	public void testRPSWinner_aWin() {
+		assertEquals("Player A Wins", RPS.rpsWinner("RS, SP, RP, PR, RS"));
+	}
+	
 	//B wins
+	@Test
+	public void testRPSWinner_bWin() {
+		assertEquals("Player B Wins", RPS.rpsWinner("SR, SP, RP, PS"));
+	}
+	
+	//Different Cases
+	//Draw case
+		@Test
+		public void testRPSWinner_draw_differentCase() {
+			assertEquals("Draw", RPS.rpsWinner("RS, sS, RP, rR"));
+		}
+		
+		//A wins
+		@Test
+		public void testRPSWinner_aWin_differentCase() {
+			assertEquals("Player A Wins", RPS.rpsWinner("rS, Sp, rP, PR, RS, pp"));
+		}
+		
+		//B wins
+		@Test
+		public void testRPSWinner_bWin_differentCase() {
+			assertEquals("Player B Wins", RPS.rpsWinner("sr, Sp, rp, PS, rr"));
+		}
+	
 }
