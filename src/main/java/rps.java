9c95af38c45dbd68d15b4145ef5c7072b4e3c3c6
Cory <10764424@my.uvu.edu>
2019-10-16 23:37:40 -0600

    Update rps.java
    HW submit passes test class

--- src/main/java/rps.java
+++ src/main/java/rps.java
@@ -1,6 +1,52 @@
+import java.io.IOException;
+import java.util.Scanner;
+
 class RockPaperScissors{
 	public static void main(String[] args) {
-		;
+		RockPaperScissors rsp = new RockPaperScissors();
+		String data[];
+		data = new String[3];
+		System.out.println("This is a game of rock paper scissors. \nEnter R for rock P for paper and S for scissors");
+		@SuppressWarnings("resource")
+		Scanner scanner = new Scanner(System. in);
+		for(int i = 0; i < 3; i++) {
+			int p1 = 1;
+			int p2 = 1;
+			String inputString = "";
+			
+			while(p1 == 1) {
+				System.out.println("Player One enter a move:");
+				String temp = scanner.nextLine();
+				temp.trim();
+				System.out.println(temp);
+				if(temp.equals("R") || temp.equals("P") || temp.equals("S")) {
+					inputString = temp;
+					p1 = -1;
+				}
+				else {
+					System.out.println("Invalid input please enter R for rock P for paper or S for scissors");
+					}
+				}
+			while(p2 == 1) {
+				System.out.println("Player Two enter a move:");  
+				String temp = scanner.nextLine();
+				temp.trim();
+				if(temp.equals("R") || temp.equals("P") || temp.equals("S")) {
+					inputString += temp;
+					p2 = -1;
+				}
+				else {
+					System.out.println("Invalid input please enter R for rock P for paper or S for scissors");
+					}
+				}
+		    
+			//inputString += scanner.nextLine();
+			inputString.trim();
+			data[i] = inputString;
+			System.out.println(inputString);
+		}
+		String result = rsp.shoot(data);
+		System.out.println(result);
 	}
 	public String shoot(String[] match){
 		int a = 0;
