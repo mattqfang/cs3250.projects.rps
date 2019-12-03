ed867bf6167bb620d9e021cb4b0793bccf51b2ce
zhimingzhu <10844118@my.uvu.edu>
2019-12-03 13:56:18 -0700

    project1

--- src/main/java/rps/rps.java
+++ src/main/java/rps/rps.java
@@ -9,13 +9,24 @@ public class rps {
 		
 		char AGAME;
 		char BGAME;
+		if (string.length() <= 0) {
+			System.out.println("Null input");
+			return;
+		}
+		
 		for (int i = 0; i < string.length();i++) {
 			if (string.charAt(i) == ',') {
 				//do nothing
 			}
-			else if (string.charAt(i) != ',' && string.charAt(i+1) != ',' && i+1 < string.length()) {
+			else if (string.charAt(i + 1) != ',' && i+1 < string.length()) {
 				AGAME = string.charAt(i);
 				BGAME = string.charAt(i+1);
+				/*else if (BGAME != 'R' && BGAME != 'P' && BGAME != 'S') {
+					System.out.println("Invalid Input");
+					return;
+				}*/
+				//System.out.println(AGAME);
+				//System.out.println(BGAME);
 				if (AGAME == BGAME) {
 					//draw
 					A = A + 1;
--- src/test/java/rps/test.java
+++ src/test/java/rps/test.java
@@ -1,13 +1,26 @@
 package rps;
 
+import org.junit.Test;
+
 public class test {
+	@Test
 	public void testAwin() {
 		rps.RPSGAME("RS, SP, RS, SP");
 	}
+	@Test
 	public void testBwin() {
 		rps.RPSGAME("PR, PR, RS, RS");
 	}
+	@Test
 	public void testDraw() {
 		rps.RPSGAME("PP, PP, PP, PP");
+	}
+	@Test
+	public void testWronginput() {
+		rps.RPSGAME("AA, BB, CC, DD");
+	}
+	@Test
+	public void testnull() {
+		rps.RPSGAME("");
 	}
 }
